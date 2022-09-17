09c208a7bdb7faceff520ceb79935813855acaae
MinKyung <87973583+minkk0@users.noreply.github.com>
2022-09-18 01:59:16 +0900

    [add] week1-1

---
+++ 1주차/1번.swift
@@ -0,0 +1,11 @@
+//
+//  1번.swift
+//  Swift Algorithm
+//
+//  Created by 민경 on 2022/09/18.
+//
+
+import Foundation
+func solution(_ num:Int) -> String {
+    return num % 2 != 0 ? "Odd" : "Even"
+}
