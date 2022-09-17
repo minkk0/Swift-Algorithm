22dc34ad5706b79e1039c55c725dcf4995b086e0
MinKyung <87973583+minkk0@users.noreply.github.com>
2022-09-18 01:53:07 +0900

    Initial Commit

---
+++ Swift Algorithm/Swift_AlgorithmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_AlgorithmApp.swift
+//  Swift Algorithm
+//
+//  Created by 민경 on 2022/09/18.
+//
+
+import SwiftUI
+
+@main
+struct Swift_AlgorithmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
